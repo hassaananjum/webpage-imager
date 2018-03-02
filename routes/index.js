03e52eb87b2a92a273bb99508eb9265e266a02e9
Hassaan Anjum <hassaananjum@gmail.com>
2018-03-02 12:03:39 +0500

    web page to image converter

---
+++ routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+const uuid = require('uuid');
+const path = require('path');
+const exec = require('child_process').exec;
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/getimage', (req, res, next) => {
+  const url = decodeURIComponent(req.query.url);
+  console.log(url);
+  const rastrizeLibPath = path.join(__dirname, '../lib/rasterize.js')
+  const fileName = `${uuid.v4()}.png`;
+  const outputFile = path.join(__dirname, `../public/output/${fileName}`)
+  const cmd = `phantomjs ${rastrizeLibPath} ${url} ${outputFile} "1920px"`;
+  console.log(cmd)
+  exec(cmd, (err, stdout, stderr) => {
+    if(err){
+      console.log('in err');
+      res.send(err)
+    }
+    else{
+      // res.send(`/output/${fileName}`);
+      console.log('sending redirect');
+      res.writeHead(302, {
+        'Location' : `/output/${fileName}`
+      })
+      res.end();
+    }
+  })
+  // (async function() {
+  //   const instance = await phantom.create();
+  //   const page = await phantom.create();
+  //   console.log('waiting for page');
+  //   await page.on('onResourceRequested', function(requestData) {
+  //     console.info('Requesting', requestData.url);
+  //   });
+  //   console.log('getting url');
+  //   const status = await page.open('https://www.amazon.com/');
+  //   console.log('saving image');
+  //   var render = await page.render('amazon.png');
+  //   console.log(render);
+  //   await instance.exit();
+  //   res.send('23232');
+  // })();
+})
+module.exports = router;
